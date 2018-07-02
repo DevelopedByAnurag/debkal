65a3b509fb44d04d73bb31ce37e8c51a7449f588
Anurag Batra <25814581+DevelopedByAnurag@users.noreply.github.com>
2018-07-02 13:08:32 +0530

    Update servo.sh

--- servo.sh
+++ servo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-
 clear
 RED='\033[1;31m'
 WHITE='\033[1;37m'
@@ -17,7 +16,6 @@ echo -e "                             ${RED}   (Version - 1.O)                  
 printf "${WHITE}"
 echo -e "----------------Contributer :- Anurag Batra -------------------------------------------"
 echo -e "---------------------------${ORANGE} Hindustani ${WHITE} Tech ${GREEN} Solutions ${WHITE}-------------------------------"
-echo -e "                          Kindly Read Note First by typing note                          "
 echo -e "============================================================================================================================="
 echo -e "                             This Script will convert a Debian based os to Kali Linux"
 echo -e "                                            (Tested on Debian and Ubuntu)"
@@ -25,22 +23,21 @@ echo -e "=======================================================================
 echo -e "Are you Ready to Proceed (y/n) "
 
     clear
-    echo -e "${MENU}**********************************************************${NORMAL}"
-    echo -e "-------------Welcome to Kali Linux OS Installer--------------"
-    echo -e "\t\tConverto V2.O"
-    echo -e "1)${MENU} Install Kali Linux base system"
-    echo -e "2)${MENU} Install Kali Linux - all packages"
-    echo -e "3)${MENU} Install Kali Linux forensic tools"
-    echo -e "4)${MENU} Install Kali Linux complete system"
-    echo -e "5)${MENU} Install Kali Linux GPU tools"
-    echo -e "6)${MENU} Install Kali Linux Nethunter tools"
-    echo -e "7)${MENU} Install Kali Linux password cracking tools"
-    echo -e "8)${MENU} Install Kali Linux RFID tools"
-    echo -e "9)${MENU} Install Kali Linux SDR tools"
-    echo -e "10)${MENU}Install Kali Linux Top 10 tools"
-    echo -e "11)${MENU}Install Kali Linux VoIP tools"
-    echo -e "12)${MENU}Install Kali Linux webapp assessment tools"
-    echo -e "13)${MENU}Install Kali Linux wireless tools"
-    echo -e "${MENU}*********************************************************${NORMAL}"
+    echo -e "================================================================================================================"
+    echo -e "----------------------Welcome to Kali Linux OS Installer-------------------------"
+    echo -e "1) Install Kali Linux base system"
+    echo -e "2) Install Kali Linux - all packages"
+    echo -e "3) Install Kali Linux forensic tools"
+    echo -e "4) Install Kali Linux complete system"
+    echo -e "5) Install Kali Linux GPU tools"
+    echo -e "6) Install Kali Linux Nethunter tools"
+    echo -e "7) Install Kali Linux password cracking tools"
+    echo -e "8) Install Kali Linux RFID tools"
+    echo -e "9) Install Kali Linux SDR tools"
+    echo -e "10) Install Kali Linux Top 10 tools"
+    echo -e "11) Install Kali Linux VoIP tools"
+    echo -e "12) Install Kali Linux webapp assessment tools"
+    echo -e "13) Install Kali Linux wireless tools"
+    echo -e "============================================================================================================="
     echo -e "Choose one of the Following (1-12):- "
     
