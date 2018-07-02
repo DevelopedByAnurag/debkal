17724e6dfa29c8da89dc513fbfa16f32dbd5e291
Anurag Batra <25814581+DevelopedByAnurag@users.noreply.github.com>
2018-07-02 23:00:29 +0530

    Update debkal.sh

--- debkal.sh
+++ debkal.sh
@@ -6,12 +6,12 @@ ORANGE='\033[01;38;5;166m'
 GREEN='\033[01;38;5;118m'
 echo "Task Completed Thanks For Using"
 printf "${ORANGE}"
-echo -e " ███████╗ ███████╗ ██████╗  ██╗   ██╗  ██████╗  ███████╗ ███████╗ ████████╗  "
-echo -e " ██╔════╝ ██╔════╝ ██╔══██╗ ██║   ██║ ██╔═══██╗ ██╔════╝ ██╔════╝ ╚══██╔══╝ "
-echo -e " ${WHITE}███████╗ █████╗   ██████╔╝ ██║   ██║ ██║   ██║ ███████╗ █████╗      ██║ "
-echo -e " ╚════██║ ██╔══╝   ██╔══██╗ ╚██╗ ██╔╝ ██║   ██║ ╚════██║ ██╔══╝      ██║ "
-echo -e " ${GREEN}███████║ ███████╗ ██║  ██║  ╚████╔╝  ╚██████╔╝ ███████║ ███████╗    ██║ "
-echo -e " ╚══════╝ ╚══════╝ ╚═╝  ╚═╝   ╚═══╝    ╚═════╝  ╚══════╝ ╚══════╝    ╚═╝ "
+echo -e " ███████╗  ███████╗ ██████╗  ██╗  ██╗  █████╗  ██╗"
+echo -e " ███╔══██╗ ██╔════╝ ██╔══██╗ ██║ ██╔╝ ██╔══██╗ ██║"
+echo -e "${WHITE} ███║  ██║ █████╗   ██████╔╝ █████╔╝  ███████║ ██║"
+echo -e " ███║  ██║ ██╔══╝   ██╔══██╗ ██╔═██╗  ██╔══██║ ██║"
+echo -e "${GREEN} ███████╔╝ ███████╗ ██████╔╝ ██║  ██╗ ██║  ██║ ███████╗"
+echo -e " ╚═════╝  ╚══════╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚══════╝"
 echo -e "                             ${RED}   (Version - 1.O)                                "
 printf "${WHITE}"
 echo -e "----------------Contributer :- Anurag Batra -------------------------------------------"
@@ -42,7 +42,23 @@ echo -e "Are you Ready to Proceed (y/n) "
     echo -e "Choose one of the Following (1-12):- "
     
     
-    
+    echo -e "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 40976EAF437D05B5
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
+    sudo apt-get install kali-archive-keyring --allow-unauthenticated -y
+    sudo apt-get update && apt-get upgrade --allow-unauthenticated -y && apt-get dist-upgrade -y  --allow-unauthenticated
+    apt-get -f install -y  --allow-unauthenticated
+    kali=("kali-linux" "kali-linux-all" "kali-linux-forensic" "kali-linux-full" "kali-linux-gpu" "kali-linux-nethunter" "kali-linux-pwtools" "kali-linux-rfid" "kali-linux-sdr" "kali-linux-top10" "kali-linux-voip" "kali-linux-web" "kali-linux-wireless")
+    apt-get -f install ${kali[$1-1]} -y --allow-unauthenticated
+    echo "Kali" >> /etc/hostname
+    sudo apt update -y && sudo apt upgrade -y
+    sudo apt-get install linux-headers-generic -y
+    sudo apt update -y && sudo apt upgrade -y
+    clear
+    echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -e "\n\n Installation Completed Successfully!!! Just reboot the server !!"
+    echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    read -n 1 -s -r -p "Press any key to continue To next step..."
     
     
     
@@ -87,14 +103,14 @@ do
 		echo -e "============================================================================================================================="
 		myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 		echo "Connect it!!! Using :-  ${myip}:1 in VNC Viewer"
-  echo -e "============================================================================================================================="
+		echo -e "============================================================================================================================="
 		break
 		;;
 
 	2)
   echo -e "============================================================================================================================="
-		echo -e"Okay your Wish VNC not installed"
-  echo -e "Its sad to see you you going"
+  echo -e"Okay your Wish VNC not installed"
+  echo -e "Just reboot the Machine and lets see what happens"
   echo -e "============================================================================================================================="
 		break
 		;;
