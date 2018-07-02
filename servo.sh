f84010c728c299ad1cf4dce542a8bf744947133e
Anurag Batra <25814581+DevelopedByAnurag@users.noreply.github.com>
2018-07-02 13:38:58 +0530

    Update servo.sh

--- servo.sh
+++ servo.sh
@@ -41,3 +41,63 @@ echo -e "Are you Ready to Proceed (y/n) "
     echo -e "============================================================================================================="
     echo -e "Choose one of the Following (1-12):- "
     
+    
+    
+    
+    
+    
+    
+clear
+echo -e "\nDo you want to install VNC?"
+echo -e "\n 1. Yes, Install Graphical VNC"
+echo -e "\n 2. No, I changed my Mood Exit Script"
+while :
+do
+  read INPUT_STRING
+  case $INPUT_STRING in
+	1)
+		echo -e "============================================================================================================================="
+		echo -e "                             Installing GRAPHICAL VNC (tightvncserver) "
+  		echo -e "============================================================================================================================="
+		apt-get update -y && sudo apt-get upgrade -y
+		sudo apt-get install xfce4 xfce4-goodies xorg lxde-core tightvncserver -y --allow-unauthenticated
+		vncserver
+		vncserver -kill :1
+		mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+		echo "#!/bin/bash
+		xrdb $HOME/.Xresources
+		startxfce4 &
+		lxterminal &
+		/usr/bin/lxsession -s LXDE &" >> ~/.vnc/xstartup
+		sudo chmod +x ~/.vnc/xstartup
+		echo "#!/bin/bash
+		PATH="$PATH:/usr/bin/"
+		export USER="user"
+		DISPLAY="1"
+		DEPTH="16"
+		GEOMETRY="1024x768"
+		OPTIONS="-depth ${DEPTH} -geometry ${GEOMETRY} :${DISPLAY} -localhost"
+		. /lib/lsb/init-functions" >> /etc/init.d/vncserver
+		sudo chmod +x /etc/init.d/vncserver
+		rm /etc/hostname
+		echo "Parrot OS" >> /etc/hostname
+		vncserver
+		su
+		clear
+		echo -e "============================================================================================================================="
+		myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+		echo "Connect it!!! Using :-  ${myip}:1 in VNC Viewer"
+  echo -e "============================================================================================================================="
+		break
+		;;
+
+	2)
+  echo -e "============================================================================================================================="
+		echo -e"Okay your Wish VNC not installed"
+  echo -e "Its sad to see you you going"
+  echo -e "============================================================================================================================="
+		break
+		;;
+ esac
+done
+    
